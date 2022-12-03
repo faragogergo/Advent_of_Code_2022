5d58f179f66c3a2789a00f0f0c554e1480764ba0
Faragó Gergő <geeryy@gmail.com>
2022-12-03 09:50:39 +0100

    day 2 cleanup

--- src/Day02.kt
+++ src/Day02.kt
@@ -46,7 +46,7 @@ fun main() {
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 15)
-//
+
     val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
